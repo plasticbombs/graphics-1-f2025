9ba058a488ce52aec7defd76ad904c019286e527
Smiley98 <smileysofficialcinema@gmail.com>
2025-09-10 16:32:13 -0400

    Added keyboard callback

--- src/Window.cpp
+++ src/Window.cpp
@@ -9,6 +9,12 @@ struct App
 	GLFWwindow* window = nullptr;
 } gApp;
 
+void KeyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(gApp.window, GLFW_TRUE);
+}
+
 void CreateWindow(int width, int height, const char* title)
 {
     /* Initialize the library */
@@ -28,6 +34,8 @@ void CreateWindow(int width, int height, const char* title)
 
     // Load OpenGL extensions
     assert(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress));
+
+    glfwSetKeyCallback(gApp.window, KeyboardCallback);
 }
 
 bool WindowShouldClose()
